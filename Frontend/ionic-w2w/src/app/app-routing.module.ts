4c635352588b04f3a846e9e3fb480812e0f15028
Pablo-Zuniga <pablousky44@gmail.com>
2022-02-06 21:20:54 +0100

    dev0.2.6-Login register page

--- Frontend/ionic-w2w/src/app/app-routing.module.ts
+++ Frontend/ionic-w2w/src/app/app-routing.module.ts
@@ -7,6 +7,14 @@ const routes: Routes = [
     loadChildren: () =>
       import('./tabs/tabs.module').then((m) => m.TabsPageModule),
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
 ];
 @NgModule({
   imports: [
---
+++ Frontend/ionic-w2w/src/app/login/login.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  username: string;
+  password: string;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {}
+
+  login() {
+    if (this.username && this.password) {
+      this.authService.login(this.username, this.password).subscribe((data) => {
+        data = {
+          ...data,
+          u: this.username,
+        };
+        console.log('User is logged in');
+        this.router.navigateByUrl('/');
+      });
+    }
+  }
+}
