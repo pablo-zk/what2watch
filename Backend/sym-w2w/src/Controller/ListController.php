9a6102203782e4ceab8fbf2e98e6a87fe446d266
asier_elorza <asierelorza18@gmail.com>
2022-02-06 21:18:14 +0100

    Implementacion nueva carpeta Backend con autentificacion

--- Backend/sym-w2w/src/Controller/ListController.php
+++ Backend/sym-w2w/src/Controller/ListController.php
@@ -2,12 +2,10 @@
 
 namespace App\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\ContentList;
+
 class ListController extends AbstractController
 {
     /**
--- Backend/sym-w2w/src/Controller/SecurityController.php
+++ Backend/sym-w2w/src/Controller/SecurityController.php
@@ -2,16 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
 
-class AuthController extends AbstractController
+class SecurityController extends AbstractController
 {
- /**
+    /**
      * @Route("/register", name="register", methods="post")
      */
     public function register(Request $request, UserPasswordEncoderInterface $encoder)
@@ -20,34 +21,17 @@ class AuthController extends AbstractController
         // IMP! To get JSON format from POST method
         $data = $request->getContent();
         $content = json_decode($data);
-        
         $username = $content->username;
         $password = $content->password;
-
-        $user = new User($username);
-        $user->setPassword($password);
-
+        $user = new User();
+        $user->setUsername($username);
+        $user->setPassword($encoder->encodePassword($user, $password));
         $em->persist($user);
         $em->flush();
-        return $this->json([
+        return new JsonResponse([
+            'message' => 'Usuario registrado',
             'id_token' => $user->getPassword(),
-            'message'=> "Register success"
         ]);
-    }
-    /**
-     * @Route("/role", name="role", methods="post")
-     */
-    public function role(Request $request, UserPasswordEncoderInterface $encoder)
-    {
-        $em = $this->getDoctrine()->getManager();
-        // IMP! To get JSON format from POST method
-        $data = $request->getContent();
-        $content = json_decode($data);
-        $username = $content->username;
-        $db_user = $em->getRepository(User::class)->findOneBy([
-            'username' => $username,
-        ]);
-        return new JsonResponse( $db_user->getRoles());
     }
 
     /**
