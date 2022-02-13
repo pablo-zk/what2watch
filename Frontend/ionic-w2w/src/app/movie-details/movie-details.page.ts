8ace8de42ac0cbb340c13042601e8c475a27c7cf
asier_elorza <asierelorza18@gmail.com>
2022-02-13 19:36:13 +0100

    dev0.2.13

--- Frontend/ionic-w2w/src/app/movie-details/movie-details.page.ts
+++ Frontend/ionic-w2w/src/app/movie-details/movie-details.page.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { ListService } from '../core/list.service';
 import { ActionsService } from '../services/actions.service';
+import { AuthService } from '../services/auth.service';
 import { ContentService } from '../services/content.service';
 import { MovieService } from '../services/movie.service';
 import { Content } from '../shared/content';
+import { List } from '../shared/list';
 
 @Component({
   selector: 'app-movie-details',
@@ -13,6 +17,7 @@ import { Content } from '../shared/content';
 export class MovieDetailsPage implements OnInit {
   errorMessage: string;
   content: any = [];
+  lists: any = [];
   images: any = [];
   isLoading: boolean = false;
   cont: Content = {
@@ -21,13 +26,17 @@ export class MovieDetailsPage implements OnInit {
     title: '',
     cover: '',
   };
+  
 
   constructor(
     private activatedRoute: ActivatedRoute,
+    private authService: AuthService,
     private movieService: MovieService,
     private contentService: ContentService,
     private router: Router,
-    private action: ActionsService
+    private action: ActionsService,
+    private alertCtrl: AlertController,
+    private listService: ListService
   ) {}
 
   goBack() {
@@ -48,20 +57,46 @@ export class MovieDetailsPage implements OnInit {
       this.isLoading = true;
       console.log(this.images);
     });
+    this.authService.getState().subscribe((data) => {
+      if (data != 1) {
+        //alert('Cuenta no validada');
+        this.authService.logout();
+        this.router.navigate(['login']);
+      } else {
+        this.listService.getLists().subscribe((data: List[]) => {
+          this.lists = data[0];
+        });
+        console.log(this.lists);
+      }
+    });
   }
 
-  addContent() {
+  addContent($event) {
     this.cont.idContent = this.content.id;
     this.cont.title = this.content.original_title;
     this.cont.cover = this.content.poster_path;
     console.log(this.content);
-
-    this.contentService.createContent(this.cont, '18').subscribe(
-      (data) => this.onSaveComplete(),
-      (error: any) => (this.errorMessage = <any>error)
-    );
+    $event.target.value.forEach((id) => {
+      console.log('idList: ' + id);
+      this.contentService.createContent(this.cont, id).subscribe((data) => {
+        //No hace este if. Muestra por cada lista la alerta.
+        if (data.message.startsWith('ERROR:')) {
+          this.onSaveComplete(data.message);
+        }
+      });
+    });
   }
-  onSaveComplete(): void {
-    this.router.navigate(['/tabs/tab3']);
+  onSaveComplete(message): void {
+    this.alertCtrl
+      .create({
+        header: message.startsWith('ERROR:') ? 'ERROR' : 'CORRECTO',
+        message: message,
+        buttons: ['OK'],
+      })
+      .then((res) => {
+        res.present();
+      });
+    //Se mantiene en la página por si quiere quedarse mirando los actores o mas info.
+    //this.router.navigate(['/tabs/tab3']);
   }
 }
